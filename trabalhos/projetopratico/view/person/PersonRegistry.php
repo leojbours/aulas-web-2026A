f7911f3c0161b27036f4c185dd64425039515d6c
Leonardo Bourscheid <leonardojohannbourscheid@gmail.com>
2026-06-30 19:57:31 -0300

    Ajustado encaminhamento para listagem de pessoas

--- trabalhos/projetopratico/view/person/PersonRegistry.php
+++ trabalhos/projetopratico/view/person/PersonRegistry.php
@@ -14,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $controller->save();
     }
-    header("Location: ./PesonList.php");
+    header("Location: ./PersonList.php");
     exit;
 }
 ?>
